3f2b3793fc2a3e429f3663001e1746119009cec3
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-20 10:47:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import Counter from "./component/Counter";
+import { CounterApp } from "./projects/CounterApp";
+// import MyComponent from "./component/MyComponent";
+
+function App() {
+  return (
+    <>
+      {/* <MyComponent name="Nikku" age={21} /> */}
+      {/* <Counter /> */}
+      <CounterApp />
+    </>
+  );
+}
+
+export default App;
