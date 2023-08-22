96a32cf1bc004721c323459beb055e1ed7965576
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-22 09:02:49 +0530

    to-do-list commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
 
 export const App: React.FC = () => {
-
-  const year = new Date().getFullYear();
-
   return (
     <>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <div className="footer">CopyRight &copy; {year}</div>
-        </div>
-      </Router>
+      <h1>To-do-List App</h1>
     </>
   );
 };
