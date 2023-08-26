3d59c97fd8d9230d6543a9b5a836f95e8c6e3b61
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-26 07:28:11 +0530

    shop-list commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import ContactList from "./components/ContactList";
-import { ContactData } from "./components/Data/ContactData";
+import Shopping from "./components/shopping";
 
 export const App: React.FC = () => {
-  const contact = ContactData;
-
   return (
     <>
-      <div className="container">
-        <h1>Contact List</h1>
-        <ContactList contacts={contact} />{" "}
+      <div className="app">
+        <Shopping />
       </div>
     </>
   );
