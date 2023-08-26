17c3bba15bb743cdb3d3bb92e7c48e7d7993f813
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-26 12:52:21 +0530

    Quote commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import QuoteApp from "./components/QuoteApp";
 
 export const App: React.FC = () => {
   return (
     <>
       <div className="app">
-        <h1>Counter App</h1>
+        <QuoteApp />
       </div>
     </>
   );
